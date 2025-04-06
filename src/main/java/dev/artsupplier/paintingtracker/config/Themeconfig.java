878edf448f740d181faf243b831d7d447db7a2a2
ainomat <matilainen.aino1@gmail.com>
2025-04-06 19:52:47 +0300

    Initial commit

---
+++ src/main/java/dev/artsupplier/paintingtracker/config/Themeconfig.java
@@ -0,0 +1,8 @@
+package dev.artsupplier.paintingtracker.config;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("default") //use default where custom-theme is located
+public class Themeconfig implements AppShellConfigurator {
+}
